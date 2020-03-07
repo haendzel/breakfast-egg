ab48621e165199a14120eaeeb5560246a6010e8e
Filip Handzel <f.handzel@gmail.com>
2020-03-07 21:18:24 +0100

    init structure files, images

--- BreakfastEgg/ViewController.swift
+++ BreakfastEgg/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var seconds : Int!
+    var timer = Timer();
+//    var isTimerRunning = false;
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720];
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet var collectionOfImages:[UIImageView]!
+    @IBAction func eggButton(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        print(result)
+        switch (hardness) {
+          case "Soft":
+            seconds = eggTimes["Soft"]
+            break;
+         case "Medium":
+            seconds = eggTimes["Medium"]
+            break;
+        case "Hard":
+            seconds = eggTimes["Hard"]
+          default:
+            print("Fatal Error")
+            break;
+        }
+        runTimer()
     }
-
-
+    
+    func runTimer() {
+           timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+       }
+    
+    @objc func updateTimer() {
+        if seconds > 0 {
+            seconds -= 1;
+            timerLabel.text = "\(seconds!) sekund"
+        }
+    }
 }
-
